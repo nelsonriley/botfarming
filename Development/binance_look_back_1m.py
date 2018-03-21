69e0c66cd37e592fac8f0abfc6e088e25d05f51b
EC2 Default User <ec2-user@ip-172-31-1-21.us-west-2.compute.internal>
2018-03-21 15:17:19 +0000

    adding in a one minute bot

---
+++ Development/binance_look_back_1m.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python2.7
+import sys
+print 'python', sys.version
+import utility_3 as ut
+
+file_number = 0
+total_files = 1
+length = '1m'
+
+ut.run_bot_parallel(file_number, length, total_files)

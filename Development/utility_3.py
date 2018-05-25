9391942b15f8cde7024dcdb46f5bbd3f785804ad
EC2 Default User <ec2-user@ip-172-31-8-246.us-west-2.compute.internal>
2018-05-25 19:39:14 +0000

    lowered the std_increase_factor because keep having losses. Made the bot about as conservative as it has ever been. Don't really know what to do if this doesn't work. The market keeps sliding and so does profit. Profit: -0.5894

--- Development/utility_3.py
+++ Development/utility_3.py
@@ -609,11 +609,11 @@ def buy_coin(symbol, length, file_number, client, indicator_bot):
         if length == '1m':
             minutes = 1
             overall_keep_length = 15
-            max_price_to_buy_factor = .99
+            max_price_to_buy_factor = .985
             largest_bitcoin_order = .1
-            max_trades_allowed = 21
-            max_std_increase = .5
-            indicator_trade_check_length = 1
+            max_trades_allowed = 28
+            max_std_increase = 1
+            indicator_trade_check_length = 2
             part_of_bitcoin_to_use = .4*2
             gain_min = .001
             buy_price_increase_factor = 1.001
@@ -841,6 +841,7 @@ def buy_coin(symbol, length, file_number, client, indicator_bot):
                         indicator_trades_lengths = get_indicator_trade_lengths(indicator_trades_new, overall_keep_length, minutes)
                         
                         new_std_dev_increase_factor = max(0,round((float(max_trades_allowed)-float(indicator_trades_lengths[indicator_trade_check_length]))*max_std_increase/float(max_trades_allowed),2))
+                        new_std_dev_increase_factor = new_std_dev_increase_factor - 1
         
                         pickle_write('/home/ec2-user/environment/botfarming/Development/variables/std_dev_increase_factor_'+length, new_std_dev_increase_factor)
                         
@@ -877,7 +878,7 @@ def buy_coin(symbol, length, file_number, client, indicator_bot):
                         return
                     
                     
-                    if indicator_trades_lengths[indicator_trade_check_length] > max_trades_allowed:
+                    if indicator_trades_lengths[indicator_trade_check_length] > max_trades_allowed or indicator_trades_lengths[3] > 28:
                         time.sleep(120)
                         return
                     
